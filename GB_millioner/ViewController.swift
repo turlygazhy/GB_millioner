c6dcacadd35955d067e9cb78e8324c900bbda081
turlygazhy <erasul_32@mail.ru>
2022-02-10 17:28:30 +0600

    Initial Commit

---
+++ GB_millioner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB_millioner
+//
+//  Created by Ерасыл Турлыгажы on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
